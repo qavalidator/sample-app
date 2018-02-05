5f8bc9a747f5e4beaefa16e4618466eb499d3afc
Tilman Seifert <tilman.seifert@qaware.de>
2018-02-05 12:32:51 +0100

    Minor: brush-up the example

--- qa/analysis.groovy
+++ qa/analysis.groovy
@@ -8,7 +8,6 @@ analysis("Step 1: Analyze Package Architecture") {
     // output:
     printNodes(packageCycleGraph, "packageCycleNodes.txt")
     writeDot(packageGraph, "packageGraph", architecture("Package"))
-    writeFile(dependencyGraph, "dependencyGraph.json")
 }
 
 analysis("Step 2: Check for unwanted imports") {
@@ -19,25 +18,19 @@ analysis("Step 2: Check for unwanted imports") {
 }
 
 analysis("Step 3: Analyze and check T-View Architecture") {
-    // Read the given Architecture DSL file. The architecture will be available
-    // under the name defined in the Architecture DSL file; it can be accessed
-    // with: architecture("T-View")
+    // Read the given Architecture DSL file. The architecture will be available under the name
+    // defined in the Architecture DSL file; it can be accessed with: architecture("T-View")
     readArchitecture "architecture.groovy"
 
     // Use that architecture and apply it on the dependency graph.
-    // Do so twice: once for the full graph, and once only on the part of graph
-    // which represents only classes from the input scope (leaving out referenced
-    // 3rd-party classes).
+    // Do so twice: once for the full graph, and once only on the part of graph which represents
+    // only classes from the input scope (leaving out referenced 3rd-party classes).
     architectureTView = createArchitectureView(allClassesGraph, architecture("T-View"))
-    if (architectureTView == null) {
-        println "architectureTView is null"
-    }
-    architectureTViewOnInput = createArchitectureView(
-            inputClassesGraph, architecture("T-View"), "T-View-on-Input")
+    architectureTViewOnInput = createArchitectureView(inputClassesGraph, architecture("T-View"), "T-View-on-Input")
 
-    // Check all architecture rules: all relations must be covered in the architecture
-    // definition, all components must actually be implemented, and all rules in the
-    // architecture file are really used.
+    // Check all architecture rules: all relations must be covered in the architecture definition,
+    // all components must actually be implemented, and all rules in the architecture file are
+    // really used.
     checkArchitectureRules(architectureTView, architecture("T-View"))
 }
 
@@ -46,6 +39,6 @@ analysis("Step 4: Export as DOT, GraphML, and JSON") {
     writeDot(architectureTView, "architectureTView", architecture("T-View"))
     writeDot(architectureTViewOnInput, "architectureTViewOnInput", architecture("T-View"))
 
-    // this is to import it into qav-server for interactive exploration of the dependency graph
+    // this is to import it into qav-app for interactive exploration of the dependency graph
     writeFile(dependencyGraph, "dependencyGraph.json")
 }
